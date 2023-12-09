afa95fefa92b9772d7ca2a734a938b6dd1859c2d
unknown <brannongarrett77@gmail.com>
2023-12-09 17:54:50 +0100

    Added code

--- candidate.c
+++ candidate.c
@@ -7,14 +7,18 @@ typedef struct
     int votes;
 } candidate;
 
+candidate get_candidate(string prompt);
+
 int main(void)
 {
-    candidate president = get_candidate("Enter a candidate: ");
+    candidate president = get_candidate("Enter a candidate below ");
+    printf("%s\n", president.name);
+    printf("%i\n", president.votes);
 }
 
 candidate get_candidate(string prompt)
 {
-    printf("%s", prompt);
+    printf("%s\n", prompt);
 
     candidate c;
     c.name = get_string("Enter a name: ");
